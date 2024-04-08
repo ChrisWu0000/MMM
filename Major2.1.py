c93c45103353e1e50513cece9eb99ebabe5882d4
ChrisWu0000 <chriswu2222@gmail.com>
2024-04-08 14:57:32 -0400

    Update Major2.1.py

--- Major2.1.py
+++ Major2.1.py
@@ -1,13 +1,14 @@
 import pygame
+from random import *
 pygame.init()
-class spritedata:
+"""class spritedata:
   def __init__(self,width,height,x,y,speedx,speedy):
     self.w=width
     self.h=height
     self.x=float(x)
     self.y=float(y)
     self.speedx=float(speedx)
-    self.speedy=float(speedy)
+    self.speedy=float(speedy)"""
 class Bell(pygame.sprite.Sprite):
 	def __init__(self,pos,group):
 		super().__init__(group)
@@ -16,7 +17,7 @@ class Bell(pygame.sprite.Sprite):
 class Player(pygame.sprite.Sprite):
 	def __init__(self,pos,group):
 		super().__init__(group)
-		self.image = pygame.image.load('graphics/player.png').convert_alpha()
+		self.image = pygame.image.load('Enemies/DevlinDeving.png').convert_alpha()
 		self.rect = self.image.get_rect(center = pos)
 		self.direction = pygame.math.Vector2()
 		self.speed = 5
@@ -41,7 +42,10 @@ class Player(pygame.sprite.Sprite):
 	def update(self):
 		self.input()
 		self.rect.center += self.direction * self.speed
-def create_sprite(width, height, x, y,imagedata):
+class CameraGroup(pygame.sprite.Group):
+	def __init__(self):
+		super().__init__()
+"""def create_sprite(width, height, x, y,imagedata):
   sprite = pygame.sprite.Sprite()
   size = (width,height)
   position = (x,y)
@@ -82,114 +86,30 @@ cooldown=0
 timer = pygame.time.Clock() 
 sparetimer1 = pygame.USEREVENT + 1
 pygame.time.set_timer(sparetimer1,5000)
-cooldownup1 = pygame.USEREVENT + 2
+cooldownup1 = pygame.USEREVENT + 2"""
+screen = pygame.display.set_mode((1280,720))
+clock = pygame.time.Clock()
+pygame.event.set_grab(True)
+camera_group = CameraGroup()
+player = Player((640,360),camera_group)
+for i in range(20):
+	random_x = randint(1000,2000)
+	random_y = randint(1000,2000)
+	Bell((random_x,random_y),camera_group)
 
-while running:
-  clock.tick(120)
-  for event in pygame.event.get():
-    if event.type == sparetimer1:
-      spritesdata[1].speedx=0
-      spritesdata[1].speedy=0
-    if event.type == cooldownup1:
-      cooldown=0
-    if event.type == pygame.QUIT:
-      running = False
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_ESCAPE:
+				pygame.quit()
 
-    #-----PLAYER INPUTS-----
 
-    if event.type == pygame.KEYDOWN:
-      playersprite.image.fill((0,50,0))
-      if event.key == pygame.K_ESCAPE:
-        running=False
-      if event.key == pygame.K_DOWN and cooldown!=1:
-        pygame.time.set_timer(cooldownup1, 1, loops=1)
-        spritesdata[1].speedy+=0.1
-        cooldown=1
-      if event.key == pygame.K_e and cooldown!=1:
-        pygame.time.set_timer(cooldownup1, 1, loops=1)
-        speed+=0.1
-        cooldown=1
-      if event.key == pygame.K_UP and cooldown!=1:
-        pygame.time.set_timer(cooldownup1, 1, loops=1)
-        spritesdata[1].speedy-=0.1
-        cooldown=1
-      if event.key == pygame.K_RIGHT and cooldown!=1:
-        pygame.time.set_timer(cooldownup1, 1, loops=1)
-        spritesdata[1].speedx+=0.1
-        cooldown=1
-      if event.key == pygame.K_LEFT and cooldown!=1:
-        pygame.time.set_timer(cooldownup1, 1, loops=1)
-        spritesdata[1].speedx-=0.1
-        cooldown=1
-      if event.key == pygame.K_w:
-        playerdata.speedy -= speed
-        speedcheckw=speed
-      if event.key == pygame.K_s:
-        playerdata.speedy += speed
-        speedchecks=speed
-      if event.key == pygame.K_a:
-        playerdata.speedx -= speed
-        speedchecka=speed
-      if event.key == pygame.K_d:
-        playerdata.speedx += speed
-        speedcheckd=speed
-    if event.type == pygame.KEYUP:
-      if event.key == pygame.K_w:
-        playerdata.speedy += speedcheckw
-      if event.key == pygame.K_s:
-        playerdata.speedy -= speedchecks
-      if event.key == pygame.K_a:
-        playerdata.speedx += speedchecka
-      if event.key == pygame.K_d:
-        playerdata.speedx -= speedcheckd
-      if pygame.key.get_pressed() == fox:
-        playersprite.image.fill((100,50,100))
-  
-  #-----COLLISION-----
+	screen.fill('#71ddee')
 
-  playerdata.x += playerdata.speedx
-  
-  if playerdata.x+20 > spritesdata[0].x + bg_width or playerdata.x < spritesdata[0].x:
-    if playerdata.x > spritesdata[0].x:
-      playerdata.x = spritesdata[0].x+(playerdata.x-spritesdata[0].x)-speed
-    elif playerdata.x < spritesdata[0].x:
-      playerdata.x = spritesdata[0].x+(playerdata.x-spritesdata[0].x)+speed
-  for x in range(1,len(spritesdata)):
-    spritesdata[x].x+=spritesdata[x].speedx
-    if playerdata.x < spritesdata[x].x + spritesdata[x].w and playerdata.x + playerdata.w > spritesdata[x].x and playerdata.y < spritesdata[x].y + spritesdata[x].h and playerdata.y + playerdata.h > spritesdata[x].y:
-      if playerdata.x > spritesdata[x].x:
-        playerdata.x = spritesdata[x].x+(playerdata.x-spritesdata[x].x)+spritesdata[x].speedx
-      elif playerdata.x < spritesdata[x].x:
-        playerdata.x = spritesdata[x].x+(playerdata.x-spritesdata[x].x)+spritesdata[x].speedx
-      playerdata.x -= playerdata.speedx
+	camera_group.update()
+ 
 
-  playerdata.y += playerdata.speedy
-
-  if playerdata.y+20 > spritesdata[0].y + bg_height or playerdata.y < spritesdata[0].y:
-    if playerdata.y > spritesdata[0].y:
-      playerdata.y = spritesdata[0].y+(playerdata.y-spritesdata[0].y)-speed
-    elif playerdata.y < spritesdata[0].y:
-      playerdata.y = spritesdata[0].y+(playerdata.y-spritesdata[0].y)+speed
-  for x in range(1,len(spritesdata)):
-    spritesdata[x].y+=spritesdata[x].speedy
-    if playerdata.x < spritesdata[x].x + spritesdata[x].w and playerdata.x + playerdata.w > spritesdata[x].x and playerdata.y < spritesdata[x].y + spritesdata[x].h and playerdata.y + playerdata.h > spritesdata[x].y:
-      if playerdata.y > spritesdata[x].y:
-        playerdata.y = spritesdata[x].y+(playerdata.y-spritesdata[x].y)+spritesdata[x].speedy
-      elif playerdata.y < spritesdata[x].y:
-        playerdata.y = spritesdata[x].y+(playerdata.y-spritesdata[x].y)+spritesdata[x].speedy
-      playerdata.y -= playerdata.speedy
-  
-  #-----UPDATES-----
-  
-  playersprite.rect.x=playerdata.x
-  playersprite.rect.y=playerdata.y
-  for x in range(len(spriterects)):
-    spriterects[x].rect.x=spritesdata[x].x
-    spriterects[x].rect.y=spritesdata[x].y
-  camera.center = playersprite.rect.center
-  window.fill((0,)*3)
-  window.blit(spriterects[0].image,(spriterects[0].rect.x-camera.x,spriterects[0].rect.y-camera.y))
-  for sprite in spriterects[1:]:
-    window.blit(sprite.image, (sprite.rect.x - camera.x, sprite.rect.y - camera.y))
-  window.blit(playersprite.image,(140,90))
-  pygame.display.flip()
+	pygame.display.update()
+	clock.tick(60)
