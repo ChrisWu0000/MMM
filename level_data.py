e99cb20cb4ad5fbb21231967438a34d633c59a3c
OopsMoose2 <dittodemo123@gmail.com>
2024-04-23 10:56:49 -0400

    j

--- level_data.py
+++ level_data.py
@@ -1,5 +1,5 @@
 import pygame
 level_data = {
     1:{"room": pygame.image.load("Rooms/Level1.png"), "num_bell": 30, "num_sax": 0},
-    2:{"room": pygame.image.load("Rooms/Bossroom.png"), "num_bell": 30, "num_sax": 30}  
+    2:{"room": pygame.image.load("Rooms/Bossroom.png"), "num_bell": 30, "num_sax": 40}  
 }
