0020d0b63f6929c4a9952d626e32fb5e59600cd3
OopsMoose2 <dittodemo123@gmail.com>
2024-05-02 09:47:57 -0400

    23t

--- weapon_data.py
+++ weapon_data.py
@@ -5,6 +5,6 @@ weapon_data = {
   #--------
   "Basic":{"availible":True,"cost":50,"ranged":False,"damage":50,"cooldown":30,"projectiles":1,"speed":12,"duration":55,"spread":1,"sprite":"Weapons/Bullet.png","scaling":4,"image": pygame.image.load("Enemies/Bell.png")},
   "Shotgun":{"availible":True,"cost":50,"ranged":False,"damage":35,"cooldown":60,"projectiles":9,"speed":20,"duration":25,"spread":60,"sprite":"Weapons/Bullet.png","scaling":4,"image": pygame.image.load("Enemies/Sax.png")},
-  "Minigun":{"availible":True,"cost":50,"ranged":False,"damage":10,"cooldown":15,"projectiles":2,"speed":10,"duration":35,"spread":30,"sprite":"Weapons/Bullet.png","scaling":4,"image": pygame.image.load("Player/Trent.png")},
-  "Lag_Maker":{"availible":True,"cost":50,"ranged":False,"damage":200,"cooldown":1,"projectiles":20,"speed":20,"duration":25,"spread":300,"sprite":"Weapons/Bullet.png","scaling":4,"image": pygame.image.load("Enemies/DevlinDeving.png")}
+  "Minigun":{"availible":True,"cost":50,"ranged":False,"damage":10,"cooldown":15,"projectiles":3,"speed":10,"duration":35,"spread":30,"sprite":"Weapons/Bullet.png","scaling":4,"image": pygame.image.load("Player/Trent.png")},
+  "Lag_Maker":{"availible":True,"cost":50,"ranged":False,"damage":200,"cooldown":10,"projectiles":20,"speed":20,"duration":25,"spread":300,"sprite":"Weapons/Bullet.png","scaling":4,"image": pygame.image.load("Enemies/DevlinDeving.png")}
 }
