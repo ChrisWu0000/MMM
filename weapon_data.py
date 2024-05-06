dd7c595bb4ccc4f15d7347df2088e7f778decb35
OopsMoose2 <dittodemo123@gmail.com>
2024-05-06 10:02:46 -0400

    gwg

--- weapon_data.py
+++ weapon_data.py
@@ -4,7 +4,7 @@ weapon_data = {
   "sax":{"availible":False,"ranged":True,"damage":5,"cooldown":100,"projectiles":1,"speed":4,"duration":200,"spread":1,"sprite":"Weapons/Enemy_Bullet.png","scaling":0.8},
   #--------
   "Basic":{"purchased":True,"availible":True,"cost":25,"ranged":False,"damage":50,"cooldown":30,"projectiles":1,"speed":12,"duration":55,"spread":1,"sprite":"Weapons/Bullet.png","scaling":4,"image": pygame.image.load("Enemies/Bell.png")},
-  "Shotgun":{"purchased":True,"availible":True,"cost":25,"ranged":False,"damage":35,"cooldown":60,"projectiles":9,"speed":20,"duration":25,"spread":60,"sprite":"Weapons/Bullet.png","scaling":4,"image": pygame.image.load("Enemies/Sax.png")},
-  "Minigun":{"purchased":True,"availible":True,"cost":10,"ranged":False,"damage":10,"cooldown":15,"projectiles":3,"speed":10,"duration":35,"spread":30,"sprite":"Weapons/Bullet.png","scaling":4,"image": pygame.image.load("Player/Trent.png")},
-  "Lag_Maker":{"purchased":True,"availible":True,"cost":400,"ranged":False,"damage":200,"cooldown":10,"projectiles":20,"speed":20,"duration":25,"spread":300,"sprite":"Weapons/Bullet.png","scaling":4,"image": pygame.image.load("Enemies/DevlinDeving.png")}
+  "Shotgun":{"purchased":False,"availible":True,"cost":25,"ranged":False,"damage":35,"cooldown":60,"projectiles":9,"speed":20,"duration":25,"spread":60,"sprite":"Weapons/Bullet.png","scaling":4,"image": pygame.image.load("Enemies/Sax.png")},
+  "Minigun":{"purchased":False,"availible":True,"cost":10,"ranged":False,"damage":10,"cooldown":15,"projectiles":3,"speed":10,"duration":35,"spread":30,"sprite":"Weapons/Bullet.png","scaling":4,"image": pygame.image.load("Player/Trent.png")},
+  "Lag_Maker":{"purchased":False,"availible":True,"cost":400,"ranged":False,"damage":200,"cooldown":10,"projectiles":20,"speed":20,"duration":25,"spread":300,"sprite":"Weapons/Bullet.png","scaling":4,"image": pygame.image.load("Enemies/DevlinDeving.png")}
 }
