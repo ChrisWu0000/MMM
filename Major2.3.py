3e74c31385e1b006ecaa12763256840877b04d86
ChrisWu <chriswu2222@gmail.com>
2024-05-28 10:55:38 -0400

    started a main menu

--- Major2.3.py
+++ Major2.3.py
@@ -10,12 +10,15 @@ from prop_data import *
 from weapon_data import *
 from math import floor
 import Spritesheet
+import sys
 pygame.init()
 global bosspresent, wave
 wave = 1
 levelnum = 1
 bosspresent=False
 my_font = pygame.font.SysFont('Times', 30)
+def get_font(size):
+	return pygame.font.SysFont('Times', size)
 difficulty_mult = 1
 class Enemy(pygame.sprite.Sprite): 
 	def __init__(self, name, position):
@@ -886,7 +889,7 @@ class Bullet(pygame.sprite.Sprite):
  
 	def check_collision(self,player):
 		if self.weapon["ranged"] == True:
-			self.bullet_lifetime = self.weapon["duration"]*difficulty_mult
+			self.bullet_lifetime = min(self.weapon["duration"]*(sqrt(difficulty_mult))) 
 			if self.collisionrect.colliderect(player.collisionrect):
 					if player.dashing == False:
 						player.hp -= self.damage
@@ -923,7 +926,35 @@ class Bullet(pygame.sprite.Sprite):
 			self.kill()
 		else:
 			self.spawn_time +=1
-		
+class Button():
+	def __init__(self, image, pos, text_input, font, base_color, hovering_color):
+		self.image = image
+		self.x_pos = pos[0]
+		self.y_pos = pos[1]
+		self.font = font
+		self.base_color, self.hovering_color = base_color, hovering_color
+		self.text_input = text_input
+		self.text = self.font.render(self.text_input, True, self.base_color)
+		if self.image is None:
+			self.image = self.text
+		self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+		self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
+
+	def update(self, screen):
+		if self.image is not None:
+			screen.blit(self.image, self.rect)
+		screen.blit(self.text, self.text_rect)
+
+	def checkForInput(self, position):
+		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+			return True
+		return False
+
+	def changeColor(self, position):
+		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+			self.text = self.font.render(self.text_input, True, self.hovering_color)
+		else:
+			self.text = self.font.render(self.text_input, True, self.base_color)		
 class CameraGroup(pygame.sprite.Group):
 	global wave
 	def __init__(self):
@@ -1060,6 +1091,42 @@ def spawn(name, x, numspawn):
 			numsax = -100000
 		if name == "drum":
 			numdrum = -100000
+def main_menu():
+	while True:
+		screen.blit(pygame.image.load("Rooms/BossRoom.png"), (0, 0))
+
+		MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+		MENU_TEXT = get_font(100).render("MAIN MENU", True, "#b68f40")
+		MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+
+		PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 250), 
+							text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+		OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(640, 400), 
+							text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+		QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 550), 
+							text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+
+		screen.blit(MENU_TEXT, MENU_RECT)
+
+		for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+			button.changeColor(MENU_MOUSE_POS)
+			button.update(screen)
+		
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				sys.exit()
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+					new_level(levelnum)
+				if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
+					new_level(levelnum)
+				if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+					pygame.quit()
+					sys.exit()
+
+		pygame.display.update()
 
 
 
@@ -1137,7 +1204,7 @@ numbell = 0
 numsax = 0
 wavebar = False
 numdrum = 0
-new_level(levelnum)
+main_menu()
 meep = True
 game_pause = False
 sparetimer1 = pygame.USEREVENT + 1
