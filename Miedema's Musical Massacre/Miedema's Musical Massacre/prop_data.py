f9be650a3b0bed0a25e4f5ae4803fc113944e8ff
ChrisWu <chriswu2222@gmail.com>
2024-06-18 10:06:49 -0400

    does it work?

--- Miedema's Musical Massacre/Miedema's Musical Massacre/prop_data.py
+++
@@ -1,5 +0,0 @@
-import pygame
-prop_data = {
-    "Pillar":{"image": pygame.image.load("Props/Column.png"), "collectable": False, "coin_num":0},
-    "Coin":{"image": pygame.image.load("Props/Coin.png"), "collectable": True, "coin_num": 1},
-}
